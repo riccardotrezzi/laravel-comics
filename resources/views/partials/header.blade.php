38f832da54a6a8e8e8ab9ca9bc926542c8d40a39
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-10-21 15:54:30 +0200

    aggiunti file scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 
 
 <header>
-    <div class="container">
+    <div class="container d-flex">
         <span>
             <figure>
                 <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo">
@@ -17,6 +17,17 @@
                     </li>
                     @endforeach
                 </ul>
+
+                <div class="navbar">
+                    <div class="container-fluid">
+                        <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-dark " type="submit">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                        </form>
+                    </div>
+                </div>
             </nav>
         </span>
     </div>
